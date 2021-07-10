85ed534b0855b72b17286022caef34eefa823531
Alperen Serdar Gürbüz <alperen.25.serdar@gmail.com>
2021-07-10 09:50:36 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("bu benim uygulamam");
+
+//package.json da main bölümünde app.js var.
+//start şeklinde komuta karşılık gelen değeri app.js olarak oluşturdum (npm run start olarak çalıştırabiliriz)
+
+
+
+//biz burada node modules silersek bağımlılıklarımızın isimleri package.json içinde bulunmaya devam edecek
+//npm install ı yalın halde kullanırsak bağımlılıklarımızdaki modulleri yükleyecek (dependencies)
+
+// Geliştirme ortamında ihtiyacımız varsa dev dependencies e göre kuracaz(npm install wepback-cli --save-dev)
